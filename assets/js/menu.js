b296f77d7f1195195cf982e47cf962eb3214a6be
Truowng <quockhanhtruong166@gmail.com>
2023-07-28 16:46:25 +0700

    fix repsonsive mobile bug

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,5 +1,7 @@
 $(document).ready(() => {
   $("#header .header-nav-button").click(() => {
-    $("#header .header-nav.mobile").toggle();
+    $("#header .header-nav.mobile").hasClass("active")
+      ? $("#header .header-nav.mobile").removeClass("active")
+      : $("#header .header-nav.mobile").addClass("active");
   });
 });
