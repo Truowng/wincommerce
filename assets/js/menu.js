0e120c9834cef5922f5741d98b550b51abab7ae8
Truowng <quockhanhtruong166@gmail.com>
2023-08-02 09:49:45 +0700

    add careers page + modal + search input

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -4,4 +4,22 @@ $(document).ready(() => {
       ? $("#header .header-nav.mobile").removeClass("active")
       : $("#header .header-nav.mobile").addClass("active");
   });
+
+  $("#header .header-nav .header-button-container .search-button").click(() => {
+    if (
+      $("#header .header-nav .header-button-container .search-button").hasClass(
+        "active"
+      )
+    ) {
+      $(
+        "#header .header-nav .header-button-container .search-button"
+      ).removeClass("active");
+      $("#header .header-content .header-nav").css("gap", "70px");
+    } else {
+      $("#header .header-nav .header-button-container .search-button").addClass(
+        "active"
+      );
+      $("#header .header-content .header-nav").css("gap", "47px");
+    }
+  });
 });
