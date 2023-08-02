38bf3697dcbf6c42569cde72750fe68e2d8a3e6d
Truowng <quockhanhtruong166@gmail.com>
2023-08-02 11:06:57 +0700

    add contact page + optimized contact form

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -5,21 +5,23 @@ $(document).ready(() => {
       : $("#header .header-nav.mobile").addClass("active");
   });
 
-  $("#header .header-nav .header-button-container .search-button").click(() => {
-    if (
-      $("#header .header-nav .header-button-container .search-button").hasClass(
-        "active"
-      )
-    ) {
-      $(
-        "#header .header-nav .header-button-container .search-button"
-      ).removeClass("active");
-      $("#header .header-content .header-nav").css("gap", "70px");
-    } else {
-      $("#header .header-nav .header-button-container .search-button").addClass(
-        "active"
-      );
-      $("#header .header-content .header-nav").css("gap", "47px");
+  $("#header .header-nav .header-button-container .search-button .icon").click(
+    () => {
+      if (
+        $(
+          "#header .header-nav .header-button-container .search-button"
+        ).hasClass("active")
+      ) {
+        $(
+          "#header .header-nav .header-button-container .search-button"
+        ).removeClass("active");
+        $("#header .header-content .header-nav").css("gap", "70px");
+      } else {
+        $(
+          "#header .header-nav .header-button-container .search-button"
+        ).addClass("active");
+        $("#header .header-content .header-nav").css("gap", "47px");
+      }
     }
-  });
+  );
 });
